d38f55e0906e07d89fb3f5842201948f1aa899f6
jshop911 <121673517+jshop911@users.noreply.github.com>
2023-01-09 16:01:02 -0900

    Create Colors.ts

---
+++ constants/Colors.ts
@@ -0,0 +1,19 @@
+const tintColorLight = "#faac2a";
+const tintColorDark = "#faac2a";
+
+export default {
+	light: {
+		text: "#223447",
+		background: "#fff",
+		tint: tintColorLight,
+		tabIconDefault: "#223447",
+		tabIconSelected: tintColorLight,
+	},
+	dark: {
+		text: "#fff",
+		background: "#010C01",
+		tint: tintColorDark,
+		tabIconDefault: "#223447",
+		tabIconSelected: tintColorDark,
+	},
+};
